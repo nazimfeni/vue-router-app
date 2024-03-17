2f2c5e71e30b57f228dc9c146445bac40a258304
nazimfeni <mdnazimuddin77@gmail.com>
2024-03-17 15:12:26 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <RouterLink to="/portfolio">portfolio</RouterLink>
+  </div>
+  <router-view/>
+</template> 
+<style>
+</style>
